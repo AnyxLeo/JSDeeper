270dedccfd6fa35353b96f39f70ec9cb64f2d552
unknown <anyx.leo@gmail.com>
2019-02-27 22:16:46 -0600

    Operators parte I

---
+++ MoshHamedani/BasicOperatos.js
@@ -0,0 +1,50 @@
+
+let x = 10;
+let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y); //ptencia
+
+//Increment (++)
+console.log(++x);
+console.log(x++);
+console.log(x);
+
+//Decrement (--)
+console.log(--x);
+console.log(x--);
+console.log(x);
+
+/** Assigment Operators */
+x = 10;
+
+x += 5;
+x *= 3;
+x -= 3;
+
+/**Comparison operators */
+
+x = 1;
+
+// Relational
+console.log(x > 0);
+console.log(x >= 0);
+console.log(x < 0);
+console.log(x <= 0);
+
+//Equality
+console.log(x === 1);
+console.log(x !== 1);
+
+//Strict  Equality ( Type + Value)
+console.log(1 === 1);
+console.log(1 === '1');
+
+// Lose Equality
+console.log(1 == 1);
+console.log('1' == 1);
+console.log(true == 1);
